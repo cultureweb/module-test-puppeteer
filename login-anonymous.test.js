1be97acace5aa75240a09988b0ed25f509c8f2ba
Cédric <cedric.milius@yahoo.fr>
2020-03-31 17:33:49 +0200

    test login ok

--- login-anonymous.test.js
+++ login-anonymous.test.js
@@ -30,27 +30,55 @@ describe("Login Anonymous", async () => {
         await page.waitForSelector("#navbar li a");
         await page.evaluate(() => {
             Array
-                .from(document.querySelectorAll('#navbar li a'))
-                .filter(el => el.textContent === 'Sign In')[0].click();
+                .from(document.querySelectorAll('#navbar li a.dropdown-toggle'))
+                .filter(el => el.textContent === 'Sign In ')[0].click();
         });
+       
 
         await page.waitForSelector('#dropdown', { visible: true });
-        await page.type("input[placeholder='username']", 'fakename');
+        await page.screenshot({ path: './tests/img/login.png' });
+        await page.type('input[name=username]', 'fakename');
         await page.screenshot({ path: './tests/img/login-wrong-username.png' });
-        await page.type("input[placeholder='password']", 'test');
+        await page.type("input[name='password']", 'test');
         await page.screenshot({ path: './tests/img/login-wrong-email.png' });
 
         await page.$eval('.login-form-submit', el => el.click());
         await page.screenshot({ path: './tests/img/login-modal-error.png' });
         await page.waitForSelector('body');
         const url = page.url();
-        console.log({url})
         expect(url).toBe("http://polr.web-74.com/login");
 
-    
-        //expect(errorAlertEmail).toBe("Invalid email address");
 
     }, timeout);
+
+    // login with right email and password
+    test('Login with success', async () => {
+        await page.goto('http://polr.web-74.com');
+
+        await page.waitForSelector("#navbar li a");
+        await page.evaluate(() => {
+            Array
+                .from(document.querySelectorAll('#navbar li a.dropdown-toggle'))
+                .filter(el => el.textContent === 'Sign In ')[0].click();
+        });
+
+    await page.waitForSelector('#dropdown', { visible: true });
+    await page.screenshot({ path: './tests/img/login.png' });
+    await page.type('input[name=username]', 'admin');
+    await page.screenshot({ path: './tests/img/login-right-username.png' });
+    await page.type("input[name='password']", 'campus');
+    await page.screenshot({ path: './tests/img/login-right-email.png' });
+ 
+        await page.$eval('.login-form-submit', el => el.click());
+        await page.screenshot({ path: './tests/img/login-modal-sucess.png' });
+        const test = await page.waitForSelector('.login-name');
+        
+        //on récupère le code HTML
+        const html = await page.$eval('.login-name', e => e.innerHTML);
+        //on vérifie qu'il contient la bonne chaîne de caractères
+        expect(html).toContain("admin");
+
+     }, timeout);
 
 
     // cette fonction est lancée avant chaque test de cette
